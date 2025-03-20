38f94cded38df397e3319f8fa08668af13566112
Glen Donn B. Alpasan <62196184+glentooth00@users.noreply.github.com>
2025-03-20 10:30:30 +0800

    environment setup for development

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$page_title = 'Page title';
+
+include_once('public/includes/header/header.php');
+
+?>
+
+
+
+
+<?php include_once('public/includes/footer/footer.php'); ?>
---
+++ public/includes/header/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title ?></title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="resources/images/favicon2.ico" type="image/x-icon">
+
+    <!-- bootstrap -->
+    <link href="resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap JS & Dependencies -->
+    <script src="resources/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Alpine Js --->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</head>
+<body>
